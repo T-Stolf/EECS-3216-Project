b52a2250ea601c924a6805c13c3a40fac98da21f
T-Stolf <thomsstolf@gmail.com>
2022-04-04 23:33:21 -0400

    Attempted fix of the scoring bug
    debouncer created, double debouncer may be needed. Colour error left unchanged

--- scorer.sv
+++ scorer.sv
@@ -1,13 +1,44 @@
+//module scorer (GameOver, clk, rst, answer, HEX5, HEX4);
+//input clk, rst, GameOver;
+//input logic answer;
+//output [6:0] HEX5, HEX4;
+//
+//logic[6:0] score = 7'd0;
+//
+//sevenseg A(score/10, HEX5);
+//sevenseg B(score%10, HEX4);
+//
+//
+//always_ff@(posedge clk or negedge rst) begin
+//	if(!rst) 
+//	begin
+//		score <= 7'd0; 
+//	end
+//	else if (!GameOver)
+//	begin
+//		if(answer == 1 && score < 100) 
+//		begin
+//			score <= score + 1;
+//		end
+//		else begin
+//			score <= score;
+//		end	
+//	end
+//end
+//
+//endmodule
+
 module scorer (GameOver, clk, rst, answer, HEX5, HEX4);
 input clk, rst, GameOver;
 input logic answer;
 output [6:0] HEX5, HEX4;
 
-logic[6:0] score = 7'd0;
+reg answer_out;
+
+reg [6:0] score = 7'd0;
 
 sevenseg A(score/10, HEX5);
 sevenseg B(score%10, HEX4);
-
 
 always_ff@(posedge clk or negedge rst) begin
 	if(!rst) 
@@ -16,14 +47,15 @@ always_ff@(posedge clk or negedge rst) begin
 	end
 	else if (!GameOver)
 	begin
-		if(answer == 1 && score < 100) 
-		begin
-			score <= score + 1;
-		end
-		else begin
-			score <= score;
-		end	
+		if(answer > answer_out) begin
+			if(answer == 1 && score < 100) 
+			begin
+				score <= score + 1;
+			end
+		answer_out <= answer;
 	end
+	answer_out <= answer;
+end
 end
 
 endmodule
